402b032325bb2c3f08b5d297609517a1cf1db604
nayanApt <nayanjd@aptsoftware.com>
2020-04-14 20:31:06 +0530

    Added score LiveData to score_fragment

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -53,10 +53,8 @@ class ScoreFragment : Fragment() {
         )
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(ScoreViewModel::class.java)
-        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
         binding.scoreViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
             if (playAgain)  {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
